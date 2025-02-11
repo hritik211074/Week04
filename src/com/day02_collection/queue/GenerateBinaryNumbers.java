5c17f15a311e02ab5c0e79d9986eda10625f3bc7
hritik211074 <0111al211074hritikkumarmaurya@gmail.com>
2025-02-11 16:50:22 +0530

    [Hritik] adding the file where i storing the binary

--- src/com/day02_collection/queue/GenerateBinaryNumbers.java
+++ src/com/day02_collection/queue/GenerateBinaryNumbers.java
@@ -1,4 +1,17 @@
 package com.day02_collection.queue;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class GenerateBinaryNumbers {
+    public static void main(String[] args) {
+        // Create a scanner's object to read the user input
+        Scanner sc=new Scanner(System.in);
+
+        Queue<Integer> qq=new PriorityQueue<>();
+
+        System.out.println("Enter the number of element :");
+        int num=sc.nextInt();
+    }
 }
