9c0f53c2dc139b52902f59e80104271d5a49bf44
hritik211074 <0111al211074hritikkumarmaurya@gmail.com>
2025-02-11 16:27:11 +0530

    [Hritik] added the file where i am reversing the queue

---
+++ src/com/day02_collection/reverse_queue/ReverseQueue.java
@@ -0,0 +1,38 @@
+package com.day02_collection.reverse_queue;
+
+import java.util.*;
+public class ReverseQueue {
+
+    static void findReverseQueue(Queue<Integer>qq){
+        Stack<Integer> vq = new Stack<>();
+
+        // Move all elements from queue to stack
+        while (!qq.isEmpty()) {
+            vq.push(qq.poll());
+        }
+
+        // Move all elements back from stack to queue
+        while (!vq.isEmpty()) {
+            qq.add(vq.pop());
+        }
+    }
+    public static void main(String[] args) {
+
+        // Create a scanner's object to read the user input
+        Scanner sc=new Scanner(System.in);
+
+        Queue<Integer>qq=new PriorityQueue<>();
+
+        System.out.println("Enter the number of element :");
+        int num=sc.nextInt();
+
+        System.out.println("Enter the values: ");
+        for(int i=0;i<num;i++) {
+            qq.add(sc.nextInt());
+        }
+        findReverseQueue(qq);
+
+        System.out.println("Reverse queue is: "+qq);
+
+    }
+}
