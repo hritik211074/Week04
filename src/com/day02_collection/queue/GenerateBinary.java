b1c1f66cd30b810dcbc62d7a1ebb00cbc4d03b2b
hritik211074 <0111al211074hritikkumarmaurya@gmail.com>
2025-02-11 19:15:31 +0530

    [Hritik] adding the file where we find the binary of all numbers from 1 to n

---
+++ src/com/day02_collection/queue/GenerateBinary.java
@@ -0,0 +1,39 @@
+package com.day02_collection.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+ public class GenerateBinary {
+    static Queue<String> binaryFind(int num) {
+        Queue<String> binary = new LinkedList<>();
+
+        for (int i = 1; i <= num; i++) { // Fix: Include 'num' itself
+            int temp = i;
+            StringBuilder sb = new StringBuilder();
+
+            while (temp != 0) {
+                sb.append((char)((temp & 1) + '0')); // Append bits (in reverse order)
+                temp = temp >> 1;
+            }
+
+            binary.add(sb.reverse().toString()); // Reverse to correct order before storing
+        }
+        return binary;  // Returning the answer
+    }
+
+    public static void main(String[] args) {
+
+        // Create the scanner object to read user input
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements: ");
+        int num = sc.nextInt(); // Store the user input
+
+        // Call method to find all the binary from 1 to num
+        Queue<String> binary = binaryFind(num);
+
+        ///  Display the result
+        System.out.println("All binary numbers: " + binary);
+    }
+}
