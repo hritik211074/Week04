c2006e4e1f65d49be8f11fc05d6be49b19ff127f
hritik211074 <0111al211074hritikkumarmaurya@gmail.com>
2025-02-15 15:21:24 +0530

    [Hritik] adding the file where validating the card number

---
+++ src/com/day05_regular_expression/ExtractCurrency.java
@@ -0,0 +1,4 @@
+package com.day05_regular_expression;
+
+public class ExtractCurrency {
+}
--- src/com/day05_regular_expression/ValidateCreditCard.java
+++ src/com/day05_regular_expression/ValidateCreditCard.java
@@ -1,24 +1,18 @@
 package com.day05_regular_expression;
 
+import java.util.Scanner;
+
 public class ValidateCreditCard {
     public static boolean isValidCard(String cardNumber) {
         return cardNumber.matches("^4\\d{15}$") || cardNumber.matches("^5\\d{15}$");
     }
 
     public static void main(String[] args) {
+        //Create a scanner object to read user input
+        Scanner sc = new Scanner(System.in);
 
-        // Visa credit card number
-        String card1 = "4123456789012345";
-
-
-        // MasterCard example
-        String card2 = "5123456789012345";
-
-        // Invalid example
-        String card3 = "6123456789012345";
+        String card1=sc.nextLine();
 
         System.out.println(isValidCard(card1) ? "Valid Card" : "Invalid Card");
-        System.out.println(isValidCard(card2) ? "Valid Card" : "Invalid Card");
-        System.out.println(isValidCard(card3) ? "Valid Card" : "Invalid Card");
     }
 }
