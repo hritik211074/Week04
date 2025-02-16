865fc41850fbb6b0bfa8db05d0f686a4990db90c
hritik211074 <0111al211074hritikkumarmaurya@gmail.com>
2025-02-16 12:46:46 +0530

    [Hritik] adding the file where test cases to verify that these methods work correctly.

---
+++ src/com/day05_junit_testing/main/java/org/example/Calculator.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.util.*;
+public class Calculator {
+    public static int addTwoNumber(int a, int b){
+        return (a+b);
+    }
+    public static int subtractTwoNumber(int a,int b){
+        return (a-b);
+    }
+    public static int multiplyTwoNumber(int a,int b){
+        return (a*b);
+    }
+    public static int divisionTwoNumber(int a,int b){
+        return (a/b);
+    }
+    public static void main(String[] args) {
+        // Create a scanner object to read user input
+        Scanner sc = new Scanner(System.in);
+
+        // Read two integer value;
+        System.out.println("Enter the two number");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+
+        // Call method to add two number
+        int add=addTwoNumber(a,b);
+        System.out.println("Division of two number "+add);
+
+        // Call method to asubtractdd two number
+        int subtract=subtractTwoNumber(a,b);
+        System.out.println("subtraction of two number "+subtract);
+
+        // Call method to multiplyTwoNumber two number
+        int mutiple=multiplyTwoNumber(a,b);
+        System.out.println("mutiply of two number "+mutiple);
+        try {
+            // Call method to division two number
+            int division = divisionTwoNumber(a, b);
+
+            System.out.println("Division of two number "+division);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ src/com/day05_junit_testing/test/java/CalculatorTest.java
@@ -0,0 +1,33 @@
+import org.example.Calculator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    Calculator calculator=new Calculator();
+
+    @Test
+    public void add(){
+        assertEquals(5,calculator.addTwoNumber(2,3));
+        assertEquals(6,calculator.addTwoNumber(3,3));
+    }
+
+    @Test
+    public void subtract(){
+        assertEquals(4,calculator.subtractTwoNumber(7,3));
+        assertEquals(6,calculator.subtractTwoNumber(9,3));
+    }
+
+    @Test
+    public void multiply(){
+        assertEquals(6,calculator.multiplyTwoNumber(2,3));
+        assertEquals(6,calculator.multiplyTwoNumber(2,3));
+    }
+
+    @Test
+    public void division(){
+        assertEquals(5,calculator.divisionTwoNumber(56,10));
+        assertEquals(6,calculator.divisionTwoNumber(20,3));
+    }
+
+}
